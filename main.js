650dbc3181cd06e452ab18075baf82c6ef9476e5
zenyhoj <joe.balingit@gmail.com>
2017-10-25 09:45:21 +0800

    added form js function to save to firebase

---
+++ main.js
@@ -0,0 +1,59 @@
+// Initialize Firebase
+var config = {
+    apiKey: "AIzaSyDdhsArfQAz92N4K3t-Oe1C83EbzIDZmI8",
+    authDomain: "contact-form-537fd.firebaseapp.com",
+    databaseURL: "https://contact-form-537fd.firebaseio.com",
+    projectId: "contact-form-537fd",
+    storageBucket: "contact-form-537fd.appspot.com",
+    messagingSenderId: "1033201094525"
+};
+firebase.initializeApp(config);
+
+// Reference messages collection
+var messagesRef = firebase.database().ref('messages');
+
+// Listen for form submit
+document.getElementById('contactForm').addEventListener('submit', submitForm);
+
+// Submit form
+function submitForm(e) {
+    e.preventDefault();
+
+    // Get values
+    var name = getInputVal('name');
+    var company = getInputVal('company');
+    var email = getInputVal('email');
+    var phone = getInputVal('phone');
+    var message = getInputVal('message');
+
+    // Save message
+    saveMessage(name, company, email, phone, message);
+
+    // Show alert
+    document.querySelector('.alert').style.display = 'block';
+
+    // Hide alert after 3 seconds
+    setTimeout(function () {
+        document.querySelector('.alert').style.display = 'none';
+    }, 3000);
+
+    // Clear form
+    document.getElementById('contactForm').reset();
+}
+
+// Function to get get form values
+function getInputVal(id) {
+    return document.getElementById(id).value;
+}
+
+// Save message to firebase
+function saveMessage(name, company, email, phone, message) {
+    var newMessageRef = messagesRef.push();
+    newMessageRef.set({
+        name: name,
+        company: company,
+        email: email,
+        phone: phone,
+        message: message
+    });
+}
